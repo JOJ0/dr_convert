1fe4fc9495d0fd02128f468889ff6af57b79d580
J0J0 T <jojo@peek-a-boo.at>
2019-06-21 09:24:40 +0200

    convert NoteON and NoteOFF for Dr.Beat
    * using send and log voids
    * some undefined behav warnings in for loops using sizeOf()
    * currently in mode 2 (dont send midi, just receive and log)

--- dr_convert.ino
+++ dr_convert.ino
@@ -6,29 +6,35 @@
 // mode 0 -> native MIDI bitrate
 // mode 1 -> serial bitrate, MIDI over hairless bridge
 // mode 2 -> serial bitrate, debugging via serial monitor
-const uint8_t mode = 0;
-const uint8_t midi_ch = 1;
-
+const uint8_t mode = 2;
+const uint8_t midi_ch = 10;
+const uint8_t midi_ch_drbeat = 11;
+const uint8_t midi_ch_volca = 10;
 const uint8_t ledPin = 13;      // LED pin on most Arduinos
 const uint8_t ledPins[5] = {9,10,11,12,13};      // LED pins for "we are ready" flashing
-
-uint8_t chrom_to_dr202[16][3] = {
-    {36,36,100},
-    {37,35,101},
-    {38,38,102},
-    {39,00,000},
-    {40,40,103},
-    {41,00,000},
-    {42,42,104},
-    {43,43,110},
-    {44,60,111},
-    {45,61,112},
-    {46,46,105},
-    {47,47,108},
-    {48,00,000},
-    {49,49,109},
-    {50,50,106},
-    {51,51,107},
+// convert mode 0 -> DRBEAT
+// convert mode 1 -> DRBEAT ALL ROLLS
+// convert mode 2 -> VOLCA
+uint8_t convert_mode = 0;
+uint8_t note_mapping[16][4] = {
+    // first 8 pads   DRBEAT             VOLCA
+    {36, 36,100,36}, // KICK 1           KICK
+    {37, 35,101,75}, // KICK 2           CLAVES
+    {38, 38,102,38}, // SNARE 1          SNARE
+    {39,102,102,39}, // ROLL SNARE 1     CLAP
+    {40, 40,103,67}, // SNARE 2          AGOGO
+    {41,103,103,41}, // ROLL SNARE 2     
+    {42, 42,104,42}, // CLOSED HH        CLOSED HH
+    {43, 43,110,43}, // HIT 3            LO TOM
+    // second 8 pads
+    {44, 60,111,44}, // PERC 1
+    {45, 61,112,45}, // PERC 2
+    {46, 46,105,46}, // OPEN HH          OPEN HH
+    {47, 47,108,47}, // HIT 2
+    {48,104,104,48}, // ROLL CLOSED HH
+    {49, 49,109,49}, // CRASH
+    {50, 50,106,50}, // HIT 1            HI TOM
+    {51, 51,107,51}, // RIDE            
 };
 
 MIDI_CREATE_INSTANCE(HardwareSerial, USBserial, MIDI); // with this method port is selectable here
@@ -70,7 +76,7 @@ void sendCCandLog(uint8_t cc_num, uint8_t cc_value, uint8_t _midi_ch, uint8_t _m
     //}
     aSerial.vvv().p("sending CC: ").pln(cc_num);
     aSerial.vvv().pln();
-    if (mode != 2)
+    if (_mode != 2)
     {
         delay(1);
         MIDI.sendControlChange(cc_num, cc_value, _midi_ch);
@@ -78,7 +84,6 @@ void sendCCandLog(uint8_t cc_num, uint8_t cc_value, uint8_t _midi_ch, uint8_t _m
     }
 }
 
-/*
 void sendNoteONandLog(uint8_t note_num, uint8_t note_vel, uint8_t _midi_ch, uint8_t _mode)
 {
     digitalWrite(ledPin, HIGH);
@@ -88,37 +93,79 @@ void sendNoteONandLog(uint8_t note_num, uint8_t note_vel, uint8_t _midi_ch, uint
     //}
     aSerial.vvv().p("sending NoteOn: ").pln(note_num);
     aSerial.vvv().pln();
-    if (mode != 2)
+    if (_mode != 2)
     {
         delay(1);
         MIDI.sendNoteOn(note_num, note_vel, _midi_ch);
         delay(5);
     }
 }
-*/
+
+void sendNoteOFFandLog(uint8_t note_num, uint8_t note_vel, uint8_t _midi_ch, uint8_t _mode)
+{
+    digitalWrite(ledPin, HIGH);
+    //for (uint8_t led = 1; led <= sizeof(ledPins); led++)
+    //{
+    //    digitalWrite(ledPins[led], LOW);
+    //}
+    aSerial.vvv().p("sending NoteOFF: ").pln(note_num);
+    aSerial.vvv().pln();
+    if (_mode != 2)
+    {
+        delay(1);
+        MIDI.sendNoteOff(note_num, note_vel, _midi_ch);
+        delay(5);
+    }
+}
 
 void handleNoteOn(byte Channel, byte PitchMidi, byte Velocity) {
     digitalWrite(ledPin, HIGH); // DEBUG LED on
-    aSerial.vvvv().p("channel PitchMidi: "); // DEBUG
-    aSerial.vvvv().p(Channel).p(" ").p(PitchMidi).p(" "); // DEBUG
+    aSerial.vvv().p("handleNoteOn: Channel, PitchMidi: "); // DEBUG
+    aSerial.vvv().p(Channel).p(" ").p(PitchMidi).p(" "); // DEBUG
 
-    for (uint8_t note_pos = 0; note_pos <= sizeof(chrom_to_dr202); note_pos++)
+    for (uint8_t note_pos = 0; note_pos <= sizeof(note_mapping); note_pos++)
     {
-        if (PitchMidi == chrom_to_dr202[note_pos][0]) // check if rcvd note (PitchMidi) 
-        {                                           // is one of the 16 chrom notes starting at 36 
-            aSerial.v().p("rcvd note: ").p(chrom_to_dr202[note_pos][0]).p(
-                             " velo: ").p(Velocity).p(" ");
-            aSerial.v().p("conv note: ").p(chrom_to_dr202[note_pos][1]).p(
-                             " velo: ").p(Velocity).p(" ");
-            aSerial.v().p("roll note: ").p(chrom_to_dr202[note_pos][2]).p(
-                             " velo: ").p(Velocity).p(" ");
-            MIDI.sendNoteOn(chrom_to_dr202[note_pos][1], Velocity, midi_ch);
+        if (PitchMidi == note_mapping[note_pos][0]) // check if rcvd note (PitchMidi) 
+        {                                           // is one of the 16 notes of our drumpad 
+            aSerial.vvv().p("rcvd NoteON: ").p(note_mapping[note_pos][0]).p(
+                               " velo: ").p(Velocity).p(" ");
+            //aSerial.vvv().p("conv NoteON: ").p(note_mapping[note_pos][1]).p(
+            //                   " velo: ").p(Velocity).p(" ");
+            //aSerial.vvv().p("roll NoteON: ").p(note_mapping[note_pos][2]).p(
+            //                 " velo: ").p(Velocity).p(" ");
+            //MIDI.sendNoteOn(note_mapping[note_pos][1], Velocity, midi_ch);
+            sendNoteONandLog(note_mapping[note_pos][1], Velocity, midi_ch, mode);
+        }
+        else
+        {
+            aSerial.vvv().p("blocking unknown NoteON: ").p(note_mapping[note_pos][0]);
         }
     }
 }
 
 void handleNoteOff(byte Channel, byte PitchMidi, byte Velocity) { // NoteOn with 0 velo is NoteOff. 
-    digitalWrite(ledPin, LOW);
+    digitalWrite(ledPin, LOW); // DEBUG LED off
+    aSerial.vvv().p("handleNoteOFF: Channel, PitchMidi: "); // DEBUG
+    aSerial.vvv().p(Channel).p(" ").p(PitchMidi).p(" "); // DEBUG
+
+    for (uint8_t note_pos = 0; note_pos <= sizeof(note_mapping); note_pos++)
+    {
+        if (PitchMidi == note_mapping[note_pos][0]) // check if rcvd note (PitchMidi) 
+        {                                           // is one of the 16 notes of our drumpad 
+            aSerial.vvv().p("rcvd NoteOFF: ").p(note_mapping[note_pos][0]).p(
+                               " velo: ").p(Velocity).p(" ");
+            //aSerial.vvv().p("conv NoteOFF: ").p(note_mapping[note_pos][1]).p(
+            //                   " velo: ").p(Velocity).p(" ");
+            //aSerial.vvv().p("roll NoteOFF: ").p(note_mapping[note_pos][2]).p(
+            //                 " velo: ").p(Velocity).p(" ");
+            //MIDI.sendNoteOn(note_mapping[note_pos][1], Velocity, midi_ch);
+            sendNoteOFFandLog(note_mapping[note_pos][1], Velocity, midi_ch, mode);
+        }
+        else
+        {
+            aSerial.vvv().p("blocking unknown NoteOFF: ").p(note_mapping[note_pos][0]);
+        }
+    }
 }
 
 void handleControlChange(byte inChannel, byte inNumber, byte inValue) {
