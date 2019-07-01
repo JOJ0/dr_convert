726ef6d3c71377e602124feb85824640a9d5e7c3
J0J0 T <jojo@peek-a-boo.at>
2019-07-01 07:57:47 +0200

    give pins a name, new mode DR_ROLLS_3, ifdef softserial

--- dr_convert.ino
+++ dr_convert.ino
@@ -2,17 +2,29 @@
 #include <advancedSerial.h>
 #include <SoftwareSerial.h>
 #define ELEMENTCOUNT(x)  (sizeof(x) / sizeof(x[0]))
+//give pins a name
+#define MODE_SWITCH_1_PIN 5
+#define MODE_SWITCH_2_PIN 6
+#define MODE_SWITCH_3_PIN 7
+#define MODE_SWITCH_1 digitalRead(MODE_SWITCH_1_PIN)
+#define MODE_SWITCH_2 digitalRead(MODE_SWITCH_2_PIN)
+#define MODE_SWITCH_3 digitalRead(MODE_SWITCH_3_PIN)
 
 //*** GLOBAL COMPILE AND RUNTIME SETTINGS START ***
 #define USBserial Serial
 #define VERBOSITY Level::vvvv
-SoftwareSerial MIDIserial(4, 2); // RX, TX
 // mode 0 -> native MIDI, NO debugging! (Serial can't be used twice)
 // mode 1 -> SoftwareSerial, debugging via serial monitor
-// mode 2 -> No Serial, just debugging via serial monitor
-const uint8_t mode = 0; // mode 1 needs SoftwareSerial, comment out below!
-//MIDI_CREATE_INSTANCE(SoftwareSerial, MIDIserial, MIDI);
-MIDI_CREATE_INSTANCE(HardwareSerial, USBserial, MIDI);
+// mode 2 -> No MIDI, just debugging via serial monitor
+#define SOFT_SERIAL_DEBUG 1
+const uint8_t mode = 1; // mode 1 needs SoftwareSerial, comment out above!
+
+#ifdef SOFT_SERIAL_DEBUG
+    SoftwareSerial MIDIserial(4, 2); // RX, TX
+    MIDI_CREATE_INSTANCE(SoftwareSerial, MIDIserial, MIDI);
+#else
+    MIDI_CREATE_INSTANCE(HardwareSerial, USBserial, MIDI);
+#endif
 // *** GLOBAL COMPILE AND RUNTIME SETTINGS END ***
 
 const uint8_t midi_ch = 10;
@@ -22,8 +34,8 @@ const uint8_t ledPin = 13;      // LED pin on most Arduinos
 //const uint8_t ledPins[5] = {9,10,11,12,13};      // LED pins for "we are ready" flashing
 enum conv_modes { BYPASS, DRBEAT, DRBEAT_ROLLS, VOLCA };
 conv_modes conv_mode = BYPASS; // initially we want bypass mode (if switch broken or something)
-const uint8_t conv_mode_sw1 = 5;
-const uint8_t conv_mode_sw2 = 6;
+//const uint8_t conv_mode_sw1 = 5;
+//const uint8_t conv_mode_sw2 = 6;
 
 bool conv_mode_switch[2][1] = { // 2 switches/bits saving the wanted convert mode
     {LOW},
@@ -47,33 +59,33 @@ struct convert_settings
 //kv.key = 1;
 //kv.value = "foo";
 
-uint8_t note_mapping[16][4] = {
-    // first 8 pads   DRBEAT             VOLCA
-    {36, 36,100,36}, // KICK 1           KICK
-    {37, 35,101,75}, // KICK 2           CLAVES
-    {38, 38,102,38}, // SNARE 1          SNARE
-    {39,102,102,39}, // ROLL SNARE 1     CLAP
-    {40, 40,103,67}, // SNARE 2          AGOGO
-    {41,103,103,41}, // ROLL SNARE 2     
-    {42, 42,104,42}, // CLOSED HH        CLOSED HH
-    {43, 43,110,43}, // HIT 3            LO TOM
-    // second 8 pads
-    {44, 60,111,44}, // PERC 1
-    {45, 61,112,45}, // PERC 2
-    {46, 46,105,46}, // OPEN HH          OPEN HH
-    {47, 47,108,47}, // HIT 2
-    {48,104,104,48}, // ROLL CLOSED HH
-    {49, 49,109,49}, // CRASH
-    {50, 50,106,50}, // HIT 1            HI TOM
-    {51, 51,107,51}, // RIDE            
+uint8_t note_mapping[16][8] = {
+    // first 8 pads   DRBEAT         DR_ROLLS  DR_ROLLS_2   DR_ROLLS_3    VOLCA   
+    {36, 36,100,36}, // KICK 1       (ALL)     (HATS)       (PERC)        KICK    
+    {37, 35,101,75}, // KICK 2                                            CLAVES  
+    {38, 38,102,38}, // SNARE 1                                           SNARE   
+    {39,102,102,39}, // ROLL SN 1              ROLL SN 1    ROLL PERC 1   CLAP    
+    {40, 40,103,67}, // SNARE 2                                           AGOGO   
+    {41,103,103,41}, // ROLL SN 2              ROLL SN 2    ROLL PERC 2           
+    {42, 42,104,42}, // CLSD HH                                           CLSD HAT 
+    {43, 43,110,43}, // HIT 3                                             LO TOM  
+    // second 8 pads                                                              
+    {44, 60,111,44}, // PERC 1                 ROLL HIT 3                         
+    {45, 61,112,45}, // PERC 2                 ROLL OP HAT                        
+    {46, 46,105,46}, // OPEN HAT                                          OP HAT
+    {47, 47,108,47}, // HIT 2                                                     
+    {48,104,104,48}, // ROLL CLSD HH           ROLL CLSD HH ROLL HIT 2            
+    {49, 49,109,49}, // CRASH                                                     
+    {50, 50,106,50}, // HIT 1                  ROLL RIDE                  HI TOM  
+    {51, 51,107,51}, // RIDE                                ROLL HIT 3            
 };
 
 
 void setup()
 {
     pinMode(ledPin, OUTPUT);
-    pinMode(conv_mode_sw1, INPUT);
-    pinMode(conv_mode_sw2, INPUT);
+    pinMode(MODE_SWITCH_1_PIN, INPUT);
+    pinMode(MODE_SWITCH_2_PIN, INPUT);
     MIDI.begin(midi_ch);  // Listen to incoming messages on given channel
     //MIDI.begin(MIDI_CHANNEL_OMNI);  // Listen to incoming messages on all channels
     //MIDI.turnThruOff();  // Listen to incoming messages on given channel
@@ -214,19 +226,23 @@ void loop()
     //settings.conv_mode = "DRBEAT_ROLLS"
     //settings.note_map = 2
     // read in 2 bit conversion mode switches
-    if (digitalRead(conv_mode_sw1) == LOW && digitalRead(conv_mode_sw2) == LOW)
+    if (MODE_SWITCH_1 == LOW && MODE_SWITCH_2 == LOW)
     {
         conv_mode = BYPASS;
     }
-    else if (digitalRead(conv_mode_sw1) == LOW && digitalRead(conv_mode_sw2) == HIGH)
+    else if (MODE_SWITCH_1 == LOW && MODE_SWITCH_2 == HIGH)
     {
         conv_mode = VOLCA;
     }
-    else if (digitalRead(conv_mode_sw1) == HIGH && digitalRead(conv_mode_sw2) == LOW)
+    else if (MODE_SWITCH_1 == HIGH && MODE_SWITCH_2 == LOW)
     {
         conv_mode = DRBEAT;
     }
-    else if (digitalRead(conv_mode_sw1) == HIGH && digitalRead(conv_mode_sw2) == HIGH)
+    else if (MODE_SWITCH_1 == HIGH && MODE_SWITCH_2 == HIGH)
+    {
+        conv_mode = DRBEAT_ROLLS;
+    }
+    else if (MODE_SWITCH_1 == HIGH && MODE_SWITCH_2 == HIGH)
     {
         conv_mode = DRBEAT_ROLLS;
     }
